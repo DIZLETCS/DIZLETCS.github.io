f8a0cc6c8ca535798b9498535284264c89f3c629
DIZLETCS <54758758+DIZLETCS@users.noreply.github.com>
2026-08-06 12:15:06 -0300

    Actualizar caché del símbolo DIZLE corregido

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 const smoothLogoStyles=document.createElement('link');
 smoothLogoStyles.rel='stylesheet';
-smoothLogoStyles.href='logo-smooth.css?v=20260806-1203';
+smoothLogoStyles.href='logo-smooth.css?v=20260806-1215';
 document.head.appendChild(smoothLogoStyles);
 
 const projects={
